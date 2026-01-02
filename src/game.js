6ebb9e38028865512a81b236808c26ee5c184116
TainanMac2021 <tainanmac2021@TainanMac2021deiMac.local>
2026-01-03 01:54:26 +0800

    UI: Card CSS Repair v3.23.9

--- src/game.js
+++ src/game.js
@@ -12,7 +12,7 @@ import { CardEngine } from './engine/CardEngine.js';
 
 class GuardiansDefenceGame {
     constructor() {
-        this.version = "v3.23.8"; // UI: Final Polish Icons
+        this.version = "v3.23.9"; // UI: Card CSS Repair
 
         // 初始化引擎
         this.cardEngine = new CardEngine(this);
