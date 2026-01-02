d020af47a24e14177ebb8a3201726a25fc64cc9e
TainanMac2021 <tainanmac2021@TainanMac2021deiMac.local>
2026-01-03 02:16:30 +0800

    UI: Player Card Icons v3.23.13

--- src/game.js
+++ src/game.js
@@ -12,7 +12,7 @@ import { CardEngine } from './engine/CardEngine.js';
 
 class GuardiansDefenceGame {
     constructor() {
-        this.version = "v3.23.12"; // UI: Fix HP Background
+        this.version = "v3.23.13"; // UI: Player Card Icons
 
         // 初始化引擎
         this.cardEngine = new CardEngine(this);
