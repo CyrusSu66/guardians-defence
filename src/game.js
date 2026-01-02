7b8ef1af485fa759f03eb83e8c934c7591a83047
TainanMac2021 <tainanmac2021@TainanMac2021deiMac.local>
2026-01-03 02:14:00 +0800

    UI: Fix HP Background v3.23.12

--- src/game.js
+++ src/game.js
@@ -12,7 +12,7 @@ import { CardEngine } from './engine/CardEngine.js';
 
 class GuardiansDefenceGame {
     constructor() {
-        this.version = "v3.23.11"; // UI: Remove HP Black BG
+        this.version = "v3.23.12"; // UI: Fix HP Background
 
         // 初始化引擎
         this.cardEngine = new CardEngine(this);
