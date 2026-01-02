641f6208ec1a5e938f73215693136b2aac93cfed
TainanMac2021 <tainanmac2021@TainanMac2021deiMac.local>
2026-01-03 02:07:03 +0800

    UI: Remove HP Black BG v3.23.11

--- src/game.js
+++ src/game.js
@@ -12,7 +12,7 @@ import { CardEngine } from './engine/CardEngine.js';
 
 class GuardiansDefenceGame {
     constructor() {
-        this.version = "v3.23.9"; // UI: Card CSS Repair
+        this.version = "v3.23.11"; // UI: Remove HP Black BG
 
         // 初始化引擎
         this.cardEngine = new CardEngine(this);
